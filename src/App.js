f64c95139f4f8c2b55bfa34663a014298479ce2f
ANISH MISHRA <anish.jaishimishra@gmail.com>
2023-02-16 23:45:25 -0800

    Added all necessary files to get started.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import useJaneHopkins from './hooks/useJaneHopkins';
 
 function App() {
+  const {entities} = useJaneHopkins();
+
+  const addPatient = async() => {
+    const addPatientResponse = await entities.patient.add({
+      name: "Test Person",
+      dob: "January 17th, 1990",
+      insuranceNumber: "123456789"
+    });
+    console.log(addPatientResponse);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={() => addPatient()}>Add Patient</button>
     </div>
   );
 }
