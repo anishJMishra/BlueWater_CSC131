c0f5cae1172b90f15dcb944c6f04437443b4f223
Ironiciron <123531769+Ironiciron@users.noreply.github.com>
2023-03-09 14:22:19 -0800

    Added TrackProgress.js
    Forgot to put it in my last commit.

---
+++ TrackProgress.js
@@ -0,0 +1,9 @@
+const TrackProgress = () => {
+    return (
+        <div className="create">
+            <h2>(Track Patient Progress Data Here)</h2>
+        </div>
+      );
+}
+ 
+export default TrackProgress;
